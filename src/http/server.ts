948e82b11bb8b8e018860e5bdecc2e660978e56b
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-09-09 20:40:28 -0300

    feat: creating server file

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,11 @@
-console.log("que passa");
+import fastify from "fastify";
+
+const app = fastify();
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server is running on port 3333");
+  });
