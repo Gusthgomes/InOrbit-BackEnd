8ba111621b4d684c85b0df0eabb4dcf7beababb4
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-09-09 21:44:23 -0300

    feat: running second migration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,10 @@
 import { pgTable, text, integer, timestamp } from "drizzle-orm/pg-core";
+import { createId } from "@paralleldrive/cuid2";
 
 export const goals = pgTable("goals", {
-  id: text("id").primaryKey(),
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
   title: text("title").notNull(),
   desiredWeeklyFrequency: integer("desired_weekly_frequency").notNull(),
   createdAt: timestamp("created_at", { withTimezone: true })
@@ -9,14 +12,14 @@ export const goals = pgTable("goals", {
     .defaultNow(),
 });
 
-// export const goalCompletions = pgTable('goal_completions', {
-//   id: text('id')
-//     .primaryKey()
-//     .$defaultFn(() => createId()),
-//   goalId: text('goal_id')
-//     .references(() => goals.id)
-//     .notNull(),
-//   createdAt: timestamp('created_at', { withTimezone: true })
-//     .notNull()
-//     .defaultNow(),
-// })
+export const goalCompletions = pgTable("goal_completions", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  goalId: text("goal_id")
+    .references(() => goals.id)
+    .notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
